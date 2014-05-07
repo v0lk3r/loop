5110f7563338ea0f73345a24399c7d300f1a3de3
Volker Haas <volker.haas@brainbot.com>
2014-05-07 08:37:44 +0200

    up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='Loop',
-    version='0.1.1',
+    version='0.1.2',
     packages=['loop'],
     install_requires=['python-inotify==0.6-test'],
     entry_points={
